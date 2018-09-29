da94d6eb8bc86092debab68ee3650a0199c6605a
Karen Li <kli19@stuy.edu>
2018-09-29 16:36:43 -0400

    further testing and strcat

--- str.c
+++ str.c
@@ -21,7 +21,14 @@ char * mystrcpy(char *dest, char *source){
 }
 
 char * mystrcat(char *dest, char *source){
-  return 0;
+  int i = mystrlen(dest);
+  while(*source){
+    printf("destination: %s\nsource:      %s\n\n", dest, source);
+    *(dest+i) = *source++;
+    i++;
+  }
+  
+  return dest;
 }
 
 
@@ -54,26 +61,46 @@ int main(){
   char s5[256] = "Hi";
 
   /*
+  printf("Testing mystrlen\n~~~~~~~~~~~~~~~~~~~~~\n");
   printf("The length of \"%s\" is: %d\n", s1, mystrlen(s1));
   printf("The length of \"%s\" is: %d\n", s2, mystrlen(s2));
   printf("The length of \"%s\" is: %d\n", s3, mystrlen(s3));
+  printf("\n~~~~~~~~~~~~~~~~~~~~~\n");
+  */
+  
+
+  /*
+  printf("Testing mystrcpy\n~~~~~~~~~~~~~~~~~~~~~");
+  printf("destination: %s\n", mystrcpy(s1,s4));
+  printf("destination: %s\n", mystrcpy(s2,s5));
+  printf("destination: %s\n", mystrcpy(s1,s5));
+  printf("\n~~~~~~~~~~~~~~~~~~~~~\n");
   */
 
-  //printf("mystrcpy(s1,s2) : %s\n", mystrcpy(s1,s4));
-  printf("destination: %s\n", mystrcpy(s2,s5));
-  //printf("mystrcpy(s1,s3) : %s\n", mystrcpy(s1,s5));
+  /*
+  printf("Testing mystrcat\n~~~~~~~~~~~~~~~~~~~~~\n");
+  printf("destination: %s\nEND\n\n", mystrcat(s3,s4));
+  printf("destination: %s\nEND\n\n", mystrcat(s3,s5));
+  printf("destination: %s\nEND\n", mystrcat(s5,s4));
+  printf("\n~~~~~~~~~~~~~~~~~~~~~\n");
+  */
+
   
   /*
+  printf("Testing mystrchr\n~~~~~~~~~~~~~~~~~~~~~\n");
   printf("%p\n", mystrchr(s1, 'H'));
   printf("%p\n", mystrchr(s1, 'e'));
   printf("%p\n", mystrchr(s1, 'O'));
   printf("%p\n", mystrchr(s1, 'x'));
+  printf("\n~~~~~~~~~~~~~~~~~~~~~\n");
   */
 
   /*
+  printf("Testing mystrcmp\n~~~~~~~~~~~~~~~~~~~~~\n");
   printf("%s compared to %s is: %d\n", s1, s2, strcmp(s1,s2));
   printf("%s compared to %s is: %d\n", s2, s3, mystrcmp(s2,s3));
   printf("%s compared to %s is: %d\n", s1, s3, mystrcmp(s1,s3));
+  printf("\n~~~~~~~~~~~~~~~~~~~~~\n");
   */
 
   return 0;
